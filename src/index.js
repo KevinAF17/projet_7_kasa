127b96f187ede368fd0d4ddc85d6bce6ba5c954c
Kevin Fernandes <17kevinf4@gmail.com>
2022-11-14 15:13:29 +0100

    Ajouts Dossiers et fichiers Composants, styles, header...

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Header from './Composants/header';
-import Banner from './Composants/banner';
-import Footer from './Composants/footer';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <Banner />
     <App />
-    <Footer />
   </React.StrictMode>
 );
