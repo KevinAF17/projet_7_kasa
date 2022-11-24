971788dd241e1fe94683c95d7894787ef74c7d13
Kevin Fernandes <17kevinf4@gmail.com>
2022-11-24 11:52:01 +0100

    Problème placement footer résolu et début développement page appartements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Home from './pages/homepage';
 import About from './pages/about-page';
+import Appartments from './pages/appartments'
 import ErrorPage from './pages/404notFound';
 import { Route, Routes } from 'react-router-dom';
 
@@ -8,6 +9,7 @@ export default function App() {
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
+            <Route path="/appartements/:id" element={<Appartments/>}/>
             <Route path="/404notFound" element={<ErrorPage/>}/>
         </Routes>
     )
